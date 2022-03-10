ffbad552f56a17ecb1d9e354d3570dab8d8b6f2d
Ekene Ezema <ekenestanekeson@gmail.com>
2022-03-10 22:31:45 +0000

    first c project

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,12 @@
-/*This prints "Programming is like a multilingual block
- * on the standard output"
+#include <stdio.h>
+
+/*This prints \"Programming is like a multilingual block
+ * on the standard output\"
+ *
  *
  *
  */
+ 
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
