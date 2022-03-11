e4305895575e54c290dd47e5c781fb52d1fb631a
Ekene Ezema <ekenestanekeson@gmail.com>
2022-03-11 13:29:41 +0000

    Correction of description

--- 0x01-variables_if_else_while/3-print_alphabet.c
+++ 0x01-variables_if_else_while/3-print_alphabet.c
@@ -9,17 +9,17 @@
 int main(void)
 {
 	char ch;
-    char CH;
+	char CH;
 
 	for (ch = 'a'; ch <= 'z'; ch++)
 	{
 		putchar(ch);
 	}
 
-    for (CH = 'A'; CH <= 'Z'; CH++)
-    {
-        putchar(CH);
-    }
+	for (CH = 'A'; CH <= 'Z'; CH++)
+	{
+		putchar(CH);
+	}
 	putchar('\n');
 	return (0);
 }
