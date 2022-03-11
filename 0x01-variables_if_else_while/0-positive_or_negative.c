e338348ba42d9f824bb0881f9f78100cc0dede53
Ekene Ezema <ekenestanekeson@gmail.com>
2022-03-11 12:51:40 +0000

    Correction of description

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,8 +1,9 @@
 #include <stdlib.h>
 #include <time.h>
 #include <stdio.h>
-/*
+/**
  *main - Prints if a random number is positive or negative
+ *
  *Description: Uses main function
  *Return: 0
  */
