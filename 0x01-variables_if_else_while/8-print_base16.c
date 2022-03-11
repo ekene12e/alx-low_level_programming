1387cb2873df0db4ba7454609ecbb2182b9bd77d
Ekene Ezema <ekenestanekeson@gmail.com>
2022-03-11 15:20:24 +0000

    Hexadecimal

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - prints alphabets
+ *
+ * Description: Long description
+ * Return: 0
+ */
+int main(void)
+{
+	int n;
+	char ch;
+
+	for (n = '0'; n <= '9'; n++)
+	{
+		putchar(n);
+	}
+	for (ch = 'a'; ch <= 'f'; ch++)
+	{
+		putchar(ch);
+	}
+	putchar(10);
+	return (0);
+}
