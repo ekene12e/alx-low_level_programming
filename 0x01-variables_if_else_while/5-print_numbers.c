29be876c104e6b96d5ba9d685811656da04c8109
Ekene Ezema <ekenestanekeson@gmail.com>
2022-03-11 13:50:46 +0000

    Correction of description

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - prints alphabets
+ *
+ * Description: Long description
+ * Return: 0
+ */
+int main(void)
+{
+	int n;
+
+	for (n = 0; n <= 9; n++)
+	{
+		printf("%i", n);
+	}
+	printf("\n");
+	return (0);
+}
