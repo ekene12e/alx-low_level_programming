a345d72c1626cf50efed31ffbc779f06c796014b
ekene12e <ekenestanekeson@gmail.com>
2022-03-11 14:12:52 +0100

    char

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - prints alphabets
+ * 
+ * Description: Long description
+ * Return: 0
+ */
+int main(void)
+{
+    char ch;
+
+    for (ch = 'a'; ch <= z; ch++)
+        {
+            putchar(ch);
+        } 
+    putchar('\n');
+    return (0);
+}
