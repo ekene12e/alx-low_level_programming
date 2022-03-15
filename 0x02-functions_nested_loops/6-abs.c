8edd3ae2413f3a508effc7c917e336b26297ab5c
ekene12e <ekenestanekeson@gmail.com>
2022-03-15 15:24:14 -0700

    Check

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+*_abs - Describes the main function
+*
+*Description:
+*@n: is the parameter
+* Return: int;
+*/
+int _abs(int, n)
+{
+	if ((n >= 0))
+	{
+		return (n);
+	}
+	else
+	{
+		return (-n);
+	}
+}
