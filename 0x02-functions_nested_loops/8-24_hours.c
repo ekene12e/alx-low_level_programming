1698d77e56b6887f29ae69db5cee22e5a601176c
ekene12e <ekenestanekeson@gmail.com>
2022-03-15 16:17:32 -0700

    Jack bauer

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+*jack_bauer - Describes the main function
+*
+*Description:
+* Return: void;
+*/
+void jack_bauer(void)
+{
+	int h;
+	int m;
+
+	for (h = 0; h < 24; h++)
+		for (m = 0; m <= 60; m++)
+			if (h < 10)
+				{
+				_putchar('0' + 0);
+				}
+			_putchar('0' + h);
+			if (m < 10)
+				{
+				_putchar('0' + 0);
+				}
+			_putchar('0' + m);
+}
