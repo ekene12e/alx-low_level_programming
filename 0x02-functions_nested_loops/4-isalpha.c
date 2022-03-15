e14b8bf69da66eb2edd3a427151f0a3d890326c2
ekene12e <ekenestanekeson@gmail.com>
2022-03-15 14:25:14 -0700

    check

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+*_islower - Describes the main function
+*
+*Description:
+*@c: is the parameter
+* Return: int;
+*/
+int _islower(int c)
+{
+	if (c >= 'a' && c <= 'Z')
+	{
+		return (1);
+	}
+
+	else
+	{
+		return (0);
+	}
+
+}
