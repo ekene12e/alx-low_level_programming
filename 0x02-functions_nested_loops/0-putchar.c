cce2113b1f16dc71405ae74e103a9b1cd6036efe
ekene12e <ekenestanekeson@gmail.com>
2022-03-15 11:21:05 -0700

    putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+*main - Describes the main function
+*
+*Description:
+*Return: 0
+*/
+int main(void)
+{
+	/*char *x = "_putchar";*/
+	printf("_putchar\n");
+	/*putchar("%c", x);*/
+	return 0;
+}
