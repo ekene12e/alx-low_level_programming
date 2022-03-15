41e11e80626e822200e14f7ce5b45227a4d2d791
ekene12e <ekenestanekeson@gmail.com>
2022-03-15 13:53:19 -0700

    check

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,20 @@
+#include "main.h"
+i/**
+*_islower - Describes the main function
+*
+*Description:
+*Return: void
+*/
+int _islower(int c)
+{
+	if (c >=97 && <= 122)
+	{
+		return 1;
+	}
+
+	else
+	{
+		return 0;
+	}
+
+}
