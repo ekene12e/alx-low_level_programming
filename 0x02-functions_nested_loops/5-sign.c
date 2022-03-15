32e4c9075b0d8cd943c5263089ab74c5da4642d7
ekene12e <ekenestanekeson@gmail.com>
2022-03-15 15:14:18 -0700

    check

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,28 @@
+#include "main.h"
+/**
+*print_sign - Describes the main function
+*
+*Description:
+*@n: is the parameter
+* Return: int;
+*/
+int print_sign(int n)
+{
+	if ((n > 0))
+	{
+		_putchar(43);
+		return (1);
+	}
+
+	else if (n == 0)
+	{
+		_putchar('0');
+		return (0);
+	}
+
+	else 
+	{
+		_putchar(45);
+		return (-1);
+	}
+}
