d83ef9c9061f2b84f24a64326b03896944e5bcf2
ekene12e <ekenestanekeson@gmail.com>
2022-03-15 17:57:12 -0700

    Jack bauer

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,26 @@
+#include "main.h"
+#include <stdio>
+/**
+*print_to_98 - Describes the main function
+*
+*Description:
+*@n: some int
+*Return: void
+*/
+void print_to_98(int n)
+{
+	if (n >= 98)
+		{
+		for (i = 98, i <= n; i++)
+		{
+			printf("%i", i);
+		}
+		}
+	else if (n < 98)
+		{
+		for (i = n; i <= 98; i++)
+		{
+			printf("%i");
+		}
+		}
+}
