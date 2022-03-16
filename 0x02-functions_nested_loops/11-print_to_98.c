04a4f6b578722dcef8c6147ab0fbdc1a3d2b12de
ekene12e <ekenestanekeson@gmail.com>
2022-03-15 18:15:01 -0700

    Lord of mercy

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -15,7 +15,7 @@ void print_to_98(int n)
 		{
 		for (i = n; i >= 98; i--)
 		{	
-			if (i = 98)
+			if (i == 98)
 				printf("%i", i);
 			else
 				printf("%i, ", i);
@@ -25,7 +25,7 @@ void print_to_98(int n)
 		{
 		for (i = n; i <= 98; i++)
 		{
-			if (i = 98)
+			if (i == 98)
 				printf("%i", i);
 			 else
 				printf("%i, ", i);
