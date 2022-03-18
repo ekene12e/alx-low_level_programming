82e3d3b8d7ae47660d46a1408dfb35bbf572357b
ekene12e <ekenestanekeson@gmail.com>
2022-03-18 12:48:44 -0700

    factor

--- 0x04-more_functions_nested_loops/100-prime_factor.c
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -9,12 +9,22 @@ int main(void)
 	long n = 612852475143;
 	long div;
 	long fact = 0;
+	long i;
 
 	for (div = 2; div < 612852475143; div++)
 	{
-		if ((n / div) == 0)
-			fact = div;
+		if ((n % div) == 0)
+		{
+			for (i = 2; i < div; i++)
+				{
+					if (div % i == 0)
+						break;
+					else
+						fact = div;
+				}
+		}
 	}
+	
 	printf("%ld\n", fact);
 	return (0);
 }
