3e0f99a5347cc64b10a65fde86c2264fca0473b3
ekene12e <ekenestanekeson@gmail.com>
2022-03-19 08:26:52 -0700

    natural

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,61 +1,49 @@
 #include "main.h"
+
 /**
-*print_times_table - prints n times table starting from 0
-*@n: n table
-*print_numbertens- prints numbers between 10 and 9et numbee
-*@a; parameter for sub
-*Return: void
-*/
-void print_numbertens(int a);
+ * print_times_table - prints the n times table, starting with 0
+ * @n: number of the times table
+ */
 void print_times_table(int n)
 {
-	int i, j, m;
+	int i, j, k;
 
-	if (n < 15 && n > 0)
+	if (n >= 0 && n <= 15)
 	{
-	for (i = 0; i <= n; i++)
-	{
-		for (j = 0; j <= n; j++)
+		for (i = 0; i <= n; i++)
 		{
-			m = j * i;
-			if (m > 9 && m < 100)
+			for (j = 0; j <= n; j++)
 			{
-				print_numbertens(m);
-			}
-			else if (m >= 100)
-			{
-				_putchar(' ');
-				_putchar((m / 100) + '0');
-				if ((m - 100) > 9)
-					_putchar(((m - 100) / 10) + '0');
-				else
-					 _putchar(0 + '0');
-				_putchar(((m - 100) % 10) + '0');
-			}
-			else
-			{
-				if (m == 0 && j == 0)
-					_putchar(m + '0');
-				else
+				k = j * i;
+				if (j == 0)
 				{
-				_putchar(' ');
-				_putchar(' ');
-				_putchar(' ');
-				_putchar((m % 10) + '0');
+
+					_putchar(k + '0');
+
+				} else if (k < 10 && j != 0)
+				{
+					_putchar(',');
+					_putchar(' ');
+					_putchar(' ');
+					_putchar(' ');
+					_putchar(k + '0');
+				} else if (k >= 10 && k < 100)
+				{
+					_putchar(',');
+					_putchar(' ');
+					_putchar(' ');
+					_putchar((k / 10) + '0');
+					_putchar((k % 10) + '0');
+				} else if (k >= 100)
+				{
+					_putchar(',');
+					_putchar(' ');
+					_putchar((k / 100) + '0');
+					_putchar(((k / 10) % 10) + '0');
+					_putchar((k % 10) + '0');
 				}
 			}
-		if (j != n)
-			_putchar(',');
-		else
 			_putchar('\n');
 		}
 	}
-	}
-}
-void print_numbertens(int a)
-{
-_putchar(' ');
-_putchar(' ');
-_putchar((a / 10) + '0');
-_putchar((a % 10) + '0');
 }
