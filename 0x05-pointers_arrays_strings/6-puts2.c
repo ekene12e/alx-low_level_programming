5258a48b73a816817358bb99a33cb706bf949d24
ekene12e <ekenestanekeson@gmail.com>
2022-03-22 16:16:27 -0700

    pointers

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+*puts2 - prints every other xter
+*@str: string
+*/
+void puts2(char *str)
+{
+	int i = 0;
+
+	whie (str[i] != '\0')
+	{
+		i++;
+	}
+	for (j = 0; j <= i; j = j + 2)
+	{
+		_putchar(str[j]);
+	}
+	_putchar('\n');
+}
