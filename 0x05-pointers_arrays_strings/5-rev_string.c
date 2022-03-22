cce2b946ddd27931fee3383ae7084d108d8b503a
ekene12e <ekenestanekeson@gmail.com>
2022-03-22 16:02:18 -0700

    pointers

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -5,19 +5,23 @@
 */
 void rev_string(char *s)
 {
-	int i = 0;
-	int j;
-	int k = 0;
+	char tmp;
+	int i, len, len1;
 
-	while (s[i] != '\0')
+	len = 0;
+	len1 = 0;
+
+	while (s[len] != '\0')
 	{
-		i++;
+		len++;
 	}
-	char *rev[i - 1];
 
-	for (j = i - 1; j >= 0; j--)
+	len1 = len - 1;
+
+	for (i = 0; i < len / 2; i++)
 	{
-		rev[k] = s[j];
-		k++;
+		tmp = s[i];
+		s[i] = s[len1];
+		s[len1--] = tmp;
 	}
 }
