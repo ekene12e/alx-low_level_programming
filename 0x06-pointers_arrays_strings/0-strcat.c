fc06795ae436492ce5673f2b1765a57504918daf
ekene12e <ekenestanekeson@gmail.com>
2022-03-24 12:16:34 -0700

    more

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,29 @@
+#include "main.h"
+/**
+**_strcat - concatenates two strings
+*@*dest: base string
+*@*src: string to be appened
+*Return: string address
+*/
+char *_strcat(char *dest, char *src)
+{
+	int s, d, len_s, len_d, i;
+
+	while (dest[len_d] != 0)
+	{
+		len_d++;
+	}
+	d = len_d - 1;
+
+	while (src[len_s != 0])
+	{
+		len_s++;
+	}
+	s = len_s - 1;
+
+	for (i = 0; i <= len_s; i++)
+	{
+		dest[len_d + i] = src[i];
+	}
+	return (*dest);
+}
