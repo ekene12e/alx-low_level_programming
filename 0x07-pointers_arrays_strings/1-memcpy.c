f5dc7f76fdde88ad398d1f2bd66230bcde1e6b56
ekene12e <ekenestanekeson@gmail.com>
2022-03-29 09:28:32 -0700

    testing

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+*_memcpy - copies a memory area
+*@dest: destination
+*@src: memory to be copied
+*@n: number of time
+*/
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	int i;
+
+	for (i = 0; i < n; i++)
+	{
+		dest[i] = src[i];
+	}
+}
+
