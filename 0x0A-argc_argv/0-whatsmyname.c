a34b309d4c1becd69f50251a59e115b16c27790a
ekene12e <ekenestanekeson@gmail.com>
2022-04-02 07:28:20 -0700

    argv

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 /**
  *main - prints name of the file
- *@argc:
- *@argv[]:
+ *@argc: size of command line arguments
+ *@argv: content of the command line
  *Return: 0
  */
 int main(int argc __attribute__((unused)), char *argv[])
