0019b9537ad602e2bc2b544e0973dbac2dedd0c4
ekene12e <ekenestanekeson@gmail.com>
2022-04-02 08:09:23 -0700

    argv

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ *main - prints name of the file
+ *@argc: size of command line arguments
+ *@argv: content of the command line
+ *Return: 0
+ */
+int main(int argc, char *argv[])
+{
+	if (argc < 3)
+	{
+		printf("Error\n");
+		return (1);
+	}else
+	{
+	printf("%d\n", (atoi(argv[1]) * atoi(argv[2])));
+	}
+	return (0);
+}
