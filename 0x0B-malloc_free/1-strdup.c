d85d58522bb385330faed23d419262f8f2f243ca
Ekene Ezema <ekenestanekeson@gmail.com>
2022-04-04 12:31:56 +0000

    malloc

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -7,17 +7,23 @@
  */
 char *_strdup(char *str)
 {
-	char *p;
-        long unsigned int i;
+	char *mem
+        unsigned int str_len = 0;
+	int i;
 
 	if (str == NULL)
 		return ('\0');
-
-	p = malloc(sizeof(str));
-	
-	for (i = 0; i <= (sizeof(p)); i++)
+	while (str[str_len] != '\0')
 	{
-		p[i] = str[i];
+	 str_len++;
 	}
+
+	mem = malloc(str_len * (sizeof(*str)));
+	
+	for (i = 0; i <= str_len; i++)
+	{
+		mem[i] = str[i];
+	}
+	mem[str_len] = '\0';
 	return (p);
 }
