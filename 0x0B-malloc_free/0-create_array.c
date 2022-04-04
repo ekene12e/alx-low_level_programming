03b1180ed4ad8836def4eaf73f1effbc3a0960ca
Ekene Ezema <ekenestanekeson@gmail.com>
2022-04-04 09:21:51 +0000

    malloc

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,16 @@
+#include <main.h>
+/**
+ *create_array - creates an array
+ *@size: size of array
+ *@c: the array
+ */
+char *create_array(unsigned int size, char c)
+{
+	if (c && size > 0)
+	{
+	char *arr = (char) *malloc(size)
+	arr[0] = c;
+	return (arr);
+	}
+	return (NULL);
+}
