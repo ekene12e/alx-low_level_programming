7c2816da9192981eed8951109cc56685af1b43b5
Ekene Ezema <ekenestanekeson@gmail.com>
2022-04-04 11:43:13 +0000

    malloc

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+#include <main.h>
+/**
+ *_strdup - returns a pointer to strings supplied
+ *@str: supplied string
+ *Return: string
+ */
+char *_strdup(char *str)
+{
+	if (str == NULL)
+		return ('\0');
+
+	*p = malloc(sizeof(str);
+	int i;
+	
+	for (i = 0; i <= (sizeof(p)); i++)
+		p[i] = str[i];
+		return (p);
+}
