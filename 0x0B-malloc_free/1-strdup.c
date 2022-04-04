3376cb19b3b581d1a7016337f1f412f84079a002
Ekene Ezema <ekenestanekeson@gmail.com>
2022-04-04 12:54:41 +0000

    malloc

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -1,28 +1,38 @@
 #include <stdlib.h>
-#include "main.h"
+
 /**
- *_strdup - returns a pointer to strings supplied
- *@str: supplied string
- *Return: string
+ * _strdup - Copies a string to new allocated space in memory
+ *
+ * @str: The string to be copied
+ *
+ * Return: Pointer to allocated memory containing copy of @str (SUCCESS)
+ * OR NULL if @str is empty (FAILURE) OR
+ * NUL if insufficient memory was available
  */
+
 char *_strdup(char *str)
 {
-	char *mem;
-	unsigned int str_len = 0;
+	char *alloc_mem;
 	unsigned int i;
+	unsigned int length_of_string = 0;
 
 	if (str == NULL)
 		return ('\0');
-	while (str[str_len] != '\0')
-	{
-		str_len++;
-	}
-	str_len++;
-	mem = malloc(str_len * (sizeof(*str)));
-	for (i = 0; i <= str_len; i++)
-	{
-		mem[i] = str[i];
-	}
-	mem[str_len] = '\0';
-	return (mem);
+
+	while (*(str + length_of_string) != '\0')
+		length_of_string++;
+
+	length_of_string++;
+
+	alloc_mem = malloc(sizeof(*str) * length_of_string);
+
+	if (alloc_mem == NULL)
+		return ('\0');
+
+	for (i = 0; i < length_of_string; i++)
+		alloc_mem[i] = *(str + i);
+
+	alloc_mem[i] = '\0';
+
+	return (alloc_mem);
 }
