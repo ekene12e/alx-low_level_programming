289c65a3b758f5c15ea22d4c79b1b73e8a13f2db
Ekene Ezema <ekenestanekeson@gmail.com>
2022-04-07 18:17:24 +0000

    alx project

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdlib.h>
+#include <stddef.h>
+/**
+ *malloc_checked - allocates a given memory space
+ *@b size of the memory
+ *Return: pointer to the The memory
+ */
+void *malloc_checked(long unsigned int b)
+{
+	char *arr;
+
+	arr = malloc(b);
+
+	if  (arr == NULL)
+		exit(98);
+
+	return (arr);
+}
---
+++ 0x0C-more_malloc_free/main.h
@@ -0,0 +1,2 @@
+int _putchar(char c);
+void *malloc_checked(long unsigned int b);
