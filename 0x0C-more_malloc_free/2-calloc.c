47bb3328bb774014c3f18dcdde1fe687b21f1086
Ekene Ezema <ekenestanekeson@gmail.com>
2022-04-08 20:10:25 +0000

    alx project

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,28 @@
+#include "main.h"
+#include <stdlib.h>
+#include <stddef.h>
+/**
+ *_calloc - allocates memory for an array
+ *@nmemb: number of members
+ *@size: size of each member
+ *
+ *Return: 
+ */
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	char *mem;
+	unsigned int i;
+
+	if (nmemb <= 0 || size <= 0)
+		return (NULL);
+
+	mem = malloc(nmemb * size);
+
+	if (mem = NULL)
+		return (NULL);
+	for (i = 0; i < nmemb; i++)
+	{
+		mem[i] = '\0';
+	}
+	return (mem);
+}
