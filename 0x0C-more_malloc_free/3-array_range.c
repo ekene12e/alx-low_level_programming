4532c6dbe1f4256875401af5a331d8c5f1a866ab
Ekene Ezema <ekenestanekeson@gmail.com>
2022-04-08 21:52:58 +0000

    alx project

---
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -0,0 +1,31 @@
+#include "main.h"
+#include <stdlib.h>
+#include <stddef.h>
+/**
+ *array_range - creates an array of integers
+ *@min: minimum int
+ *@max: maximm int
+ *
+ *Return: the created array or NULL if failure
+ */
+int *array_range(int min, int max)
+{
+	int *mem;
+	int i, len;
+
+	if (min > max)
+		return (NULL);
+
+	len = max - min + 2;
+	mem = malloc(len * sizeof(min));
+
+	if (mem == NULL)
+		return ('\0');
+
+	for (i = 0; i < len && min <= max; i++)
+	{
+		mem[i] = min;
+		min++;
+	}
+	return (mem);
+}
