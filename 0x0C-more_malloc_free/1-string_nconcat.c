ff6f07ea6b1863bec22ad38c1dfd95c21128cafd
Ekene Ezema <ekenestanekeson@gmail.com>
2022-04-08 17:33:36 +0000

    alx project

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -11,28 +11,35 @@
  */
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-	char *mem;
-	unsigned int i = 0;
-	unsigned int j, k;
+char *s;
+	unsigned int i = 0, j = 0, len1 = 0, len2 = 0;
 
-	while (s1[i] != '\0')
-	{
-		i++;
-	}
-	i = i + 2;
-	mem = malloc((i * sizeof(*s1)) + n);
-	if (mem == NULL)
+	while (s1 && s1[len1])
+		len1++;
+	while (s2 && s2[len2])
+		len2++;
+
+	if (n < len2)
+		s = malloc(sizeof(char) * (len1 + n + 1));
+	else
+		s = malloc(sizeof(char) * (len1 + len2 + 1));
+
+	if (!s)
 		return (NULL);
 
-	for (j = 0; j < (i - 2); j++)
-			{
-				mem[j] = s1[j];
-			}
-	for (k = 0; k < n; k++)
+	while (i < len1)
 	{
-		mem[j] = s2[k]
-			j++
+		s[i] = s1[i];
+		i++;
 	}
-	mem[i - 2 + n ] = '\0';
-       return (mem);	
+
+	while (n < len2 && i < (len1 + n))
+		s[i++] = s2[j++];
+
+	while (n >= len2 && i < (len1 + len2))
+		s[i++] = s2[j++];
+
+	s[i] = '\0';
+
+	return (s);
 }
