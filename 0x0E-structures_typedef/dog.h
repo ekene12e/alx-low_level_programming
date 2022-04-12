86e05f07a0a1e6bd8daa186a00ce1b5e4982f1fc
Ekene Ezema <ekenestanekeson@gmail.com>
2022-04-12 17:37:13 +0000

    alx project

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,19 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ *struct dog - a dogs infp
+ *@name: nmae of dog
+ *@age: age of dog
+ *@owner: dog owner
+ *
+ *Description: A dog prototype
+ */
+struct dog 
+	{
+	char *name;
+	float age;
+	char *owner;
+	}
+
+#endif
