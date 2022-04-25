03e8348a67cb29b4c957e0cf721dca41bf802dd5
Ekene Ezema <ekenestanekeson@gmail.com>
2022-04-25 23:06:15 +0000

    alx project

---
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -0,0 +1,24 @@
+#include "lists.h"
+
+/**
+ *get_nodeint_at_index - gets the node a particular index
+ *@head: head of the list
+ *@index: index of the list
+ *Return: Address of the node or NULL
+ *Author - Ekene Ezema
+ */
+listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
+{
+	unsigned int i = 0;
+
+	while (i <= index)
+	{
+		if (head->next == NULL && i < index)
+			return (NULL);
+		if (i == index)
+			break;
+		head = head->next;
+		i++;
+	}
+	return (head);
+}
