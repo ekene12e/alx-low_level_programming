3d6d4c84882e7da13df8645b4eb08837c087100b
Ekene Ezema <ekenestanekeson@gmail.com>
2022-05-02 20:12:24 +0000

    alx project

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -13,7 +13,7 @@ int create_file(const char *filename, char *text_content)
 	if (!filename)
 		return (-1);
 
-	file_descriptor = open(filename, O_CREAT | O_RDWR | O_TRUNCATE, 0600);
+	file_descriptor = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
 	if (text_content)
 		{
 		lseek(file_descriptor, 0, SEEK_SET);
