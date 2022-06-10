cd8dffa2c0c346b3ef0c1615c6601987b6fbe162
Ekene Ezema <ekenestanekeson@gmail.com>
2022-06-10 14:19:55 +0000

    sum of n

---
+++ 0x17-doubly_linked_lists/6-sum_dlistint.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+/**
+ *sum_dlistint - sums the n data a linked list
+ *@head: head of the list
+ *
+ *Return: sum of n or 0
+ *@Author: Ekene Ezema
+ */
+int sum_dlistint(dlistint_t *head)
+{
+	int sum;
+
+	sum = 0;
+	while (head)
+	{
+		sum += head->n;
+		head = head->next;
+	}
+	return (sum);
+}
