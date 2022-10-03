e26da3281f404343dbf1ac21b9fe534005f805ab
Ekene Ezema <ekenestanekeson@gmail.com>
2022-10-04 00:28:50 +0100

    updated task 1

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -1,6 +1,7 @@
 #ifndef SEARCH_ALGOS_H
 #define SEARCH_ALGOS_H
 #include <stdio.h>
-
+int binary_search(int *array, size_t size, int value);
 int linear_search(int *array, size_t size, int value);
+void print_array(int *a, int i, int j);
 #endif
