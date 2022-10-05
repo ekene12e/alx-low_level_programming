dc361802a321111617587806b741006824a02dd4
Malachy Ekene Ezema <52065233+ekene12e@users.noreply.github.com>
2022-10-05 22:23:39 +0100

    Update 1-binary.c

--- 0x1E-search_algorithms/1-binary.c
+++ 0x1E-search_algorithms/1-binary.c
@@ -1,58 +1,71 @@
 #include "search_algos.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
 
 /**
- * binary_search - binary search implementation
- * @array: string
- * @size: size_t
- * @value: value
- * Return: int, index of the value
+ * binary_search - searches for a value in an array of
+ * integers using the Binary search algorithm
+ * @array: array to search the value in
+ * @size: size of the array
+ * @value: value to look for
+ *
+ * Return: the index of the found value,
+ * or -1 if not found
  */
-
 int binary_search(int *array, size_t size, int value)
 {
-size_t i;
-int left = 0;
-int right = size;
-int middle;
-if (array == NULL)
-	return (-1);
+    if (!array || size == 0)
+        return (-1);
 
-for (i = 0; left < right; i++)
-{
-	printf("Searching in array: ");
-	print_array(array, left, right);
-	middle = (left + right) / 2;
-
-	if (array[middle] < value)
-		left = middle + 1;
-	else if (array[middle] > value)
-		right = middle;
-	else
-		return (middle);
+    return (help_binary(array, value, 0, size - 1));
 }
-return (-1);
-}
-
 
 /**
- * print_array - print `n` elements of an array of integers
- * @a: int array pointer to print
- * @i: int, start index
- * @j: int, end index
- * Description: Numbers must be separated by comma and space.
- * Numbers should be displayed in the same order they are stored in array.
- * You can only use _putchar to print.
+ * help_binary - searches for a value in an array of
+ * integers using recursion
+ * @array: array to search the value in
+ * @value: value to look for
+ * @lo: index of the low bound
+ * @hi: index of the high bound
+ *
+ * Return: the index of the found value,
+ * or -1 if not found
  */
-
-void print_array(int *a, int i, int j)
+int help_binary(int *array, int value, size_t lo, size_t hi)
 {
+    size_t mid;
 
-	for (; i < j; i++)
-	{
-		printf("%d", a[i]);
-		if (i < j - 1)
-			printf(", ");
-	}
-	printf("\n");
+    array_print(array, lo, hi);
+    if (hi == lo && array[lo] != value)
+        return (-1);
+
+    mid = ((hi - lo) / 2) + lo;
+    if (array[mid] == value)
+        return (mid);
+    if (array[mid] < value)
+        return (help_binary(array, value, mid + 1, hi));
+    if (array[mid] > value)
+        return (help_binary(array, value, lo, mid - 1));
+    return (-1);
 }
 
+/**
+ * array_print - prints an array
+ * @array: array to print
+ * @lo: index of the low bound
+ * @hi: index of the high bound
+ */
+void array_print(int *array, size_t lo, size_t hi)
+{
+    size_t i;
+
+    printf("Searching in array: ");
+    for (i = lo; i <= hi; i++)
+    {
+        printf("%d", array[i]);
+        if (i < hi)
+            printf(", ");
+    }
+    printf("\n");
+}
